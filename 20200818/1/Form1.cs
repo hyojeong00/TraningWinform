c36d611f47e3cd2908419a7b0dc2adb573aa4912
hyojeong00 <gywjd9563@naver.com>
2020-08-18 18:26:54 +0900

    20200818

---
+++ 20200818/1/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace _1
+{
+    public partial class Form1 : Form
+    {
+        bool bButton = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_DoubleClick(object sender, EventArgs e)
+        {
+            if (bButton==false)
+            {
+                FactButton.Text = "더블 클릭 당함";
+                bButton = true;
+            }
+            else 
+            {
+                FactButton.Text = "헬로키티 지옥으로";
+                bButton = false;
+            }
+        }
+
+        private void FactButton_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = textBox1.Text;
+        }
+
+        private void FactButton_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.Text = "마우스 치워라...";
+        }
+
+        private void FactButton_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Text = "마우스 안녕";
+        }
+
+        private void FactButton_MouseLeave(object sender, EventArgs e)
+        {
+            this.Text = "마우스 떠남";
+        }
+
+        private void FactButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+           if( e.KeyCode==Keys.Enter)
+            {
+                textBox2.Text = textBox1.Text;
+            }
+        }
+    }
+}
